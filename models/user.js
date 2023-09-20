0a022d3d2d0b47d2b99c47029a60cfec50f8cfdc
naveenbishwas <naveenbishwas4@gmail.com>
2023-09-20 22:58:21 +0530

    controllers added

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    validate: {
+      validator: function (v) {
+        return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email`,
+    },
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    required: true,
+    required: [true, "Password is required"],
+  },
+});
+
+userSchema.pre("save", async function (next) {
+  let salt = await bcrypt.genSalt();
+  this.password = await bcrypt.hash(this.password, salt);
+  console.log(this.password);
+  next();
+});
+const User = mongoose.model("User", userSchema);
+module.exports = User;
